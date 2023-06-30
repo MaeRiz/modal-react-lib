fa70df384bc656b5edcb6d31785e2bad64b9efbd
Mahery Gonin <mahery.gonin@gmail.com>
2023-06-30 22:04:54 +0200

    Create modal component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
 import React from 'react'
 import styles from './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+const Modal = ({ openState, onRequestClose, content }) => {
+
+  if (openState) {
+    document.body.style.overflow = 'hidden'
+    window.scrollTo(0, 0)
+
+    return (
+      <div className={styles.bgdark}>
+        <div className={styles.modal}>
+          <div
+            className={styles.closeBtn}
+            onClick={() => onRequestClose(false)}
+          >
+            ✖
+          </div>
+          <h1 className={styles.title}>TITLE</h1>
+          <h3 className={styles.text}>Lorem ipsum dolor sit amet. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda nesciunt praesentium amet culpa, reprehenderit harum totam corporis quia voluptatibus maxime.</h3>
+        </div>
+      </div>
+    )
+  } else {
+    document.body.style.overflow = 'auto'
+
+    return null
+  }
 }
+
+export default Modal;
